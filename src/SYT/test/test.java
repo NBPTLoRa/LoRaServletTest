dcbcd50c387c94518cedf0664606cc0bf0a7c263
sun5724 <2404960138@qq.com>
2018-11-27 18:42:42 +0800

    .

--- src/SYT/test/test.java
+++ src/SYT/test/test.java
@@ -1,6 +1,7 @@
 package SYT.test;
 
 import java.io.IOException;
+
 import java.io.InputStream;
 import java.io.Reader;
 import java.util.List;
@@ -10,6 +11,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class test {
 
 	public static void main(String[] args) throws IOException {
