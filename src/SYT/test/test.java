1524815f2f5cc65c6740187c43e45d0adfc27952
sun5724 <2404960138@qq.com>
2018-11-27 19:43:39 +0800

    修改

--- src/SYT/test/test.java
+++ src/SYT/test/test.java
@@ -2,30 +2,14 @@ package SYT.test;
 
 import java.io.IOException;
 
-import java.io.InputStream;
-import java.io.Reader;
-import java.util.List;
+import lora.sqloperation.Sql;
 
-import SYT.domain.User;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class test {
 
-	public static void main(String[] args) throws IOException {
-        String resource = "conf.xml";
-     //   InputStream is = test.class.getClassLoader().getResourceAsStream(resource);
-       // SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
-        Reader reader = Resources.getResourceAsReader(resource); 
-        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
-
-	    SqlSession session = sessionFactory.openSession(); 
-	    String start="SYT.mapping.userMapper.getAllUser";
-        List<User> use=session.selectList(start);
-        session.close();
-        System.out.println(use);
+	public static void main(String[] args){
+		Sql sql=new Sql();
+		System.out.println(sql.ALL());
 	}
 
 }
--- src/lora/sqloperation/Sql.java
+++ src/lora/sqloperation/Sql.java
@@ -1,5 +1,43 @@
 package lora.sqloperation;
 
-public class Sql {
+import java.io.IOException;
 
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.List;
+
+import SYT.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Sql {
+	SqlSession session;
+	public Sql(){
+		String resource = "conf.xml";	      
+		InputStream is = Sql.class.getClassLoader().getResourceAsStream(resource);	        
+		SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+		session = sessionFactory.openSession(); 
+	}
+	
+	public List<User> ALL()
+	{
+	    String start="SYT.mapping.userMapper.getAllUser";
+	    List<User> use=null;
+	    try
+	    {
+	    	use=session.selectList(start);
+	    }
+	    catch(Exception ex)
+	    {
+	    	use=null;
+	    }
+	    finally
+	    {
+	    	session.close();
+	    	return use;
+	    }
+	}
+	
 }
