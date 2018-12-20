595892b8806da8a19c5387281820893e3e309f14
Xy718 <869839000@qq.com>
2018-12-20 12:37:33 +0800

    doINS和DeviceADD

---
+++ src/lora/mainservlet/doIns.java
@@ -0,0 +1,69 @@
+package lora.mainservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class doIns
+ */
+public class doIns extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public doIns() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		String operID=request.getParameter("operID");
+		String operKey=request.getParameter("operKey");
+		String doOper;
+		JsonObject retJ=new JsonObject();
+		try
+		{
+			//从指令库中获取操作类-
+			doOper="";
+			//根据操作类决定函数
+			switch (doOper) {
+			case "deviceADD":
+				
+				break;
+
+			default:
+				doOper="error";
+				break;
+			}
+			
+		}
+		catch (Exception e)
+		{
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/lora/servletdo/DeviceADD.java
@@ -0,0 +1,5 @@
+package lora.servletdo;
+
+public class DeviceADD {
+
+}
